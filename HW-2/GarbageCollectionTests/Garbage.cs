6fcd4f55b42faf0e1cb5988b5b395e27f77063f3
Denis Porsev <den.porsev@gmail.com>
2020-09-15 00:31:41 +0300

    Initial commit: Task functionality added.

---
+++ HW-2/GarbageCollectionTests/Garbage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarbageCollectionTests 
+{
+    class Garbage 
+    {
+        private int id;
+
+        private bool someBoolean;
+        private int someInt;
+        private float someFloat;
+        private double someDouble;
+        private double[] arrayOfDoubles;
+
+        public Garbage(int id, int length)
+        {
+            this.id = id;
+            someBoolean = false;
+            someInt = 0;
+            someFloat = (float)2.718281828;
+            someDouble = (double)3.1415926535;
+
+            // create smth heavy weighted
+            arrayOfDoubles = new double[length];
+        }
+
+        ~Garbage()
+        {
+            Console.WriteLine("Garbage collector working on: {0}th object", id);
+            arrayOfDoubles = null;
+        }
+
+
+    }
+}
---
+++ HW-2/GarbageCollectionTests/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+
+namespace GarbageCollectionTests
+{
+    class Program
+    {
+        public static void CreateLotsOfStuff()
+        {
+            const int LENGTH = 7000;
+            const int OBJECTWEIGHT = 100;
+
+            for (int i = 1; i <= LENGTH; i++)
+            {
+                new Garbage(i, OBJECTWEIGHT);
+            }
+
+        }
+        public static void DoSomeWork()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Doing the work {0}", i);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            CreateLotsOfStuff();
+            DoSomeWork();
+
+            // Uncomment next line to benchmark the code
+            //BenchmarkRunner.Run<NameParserBenchmarks>();
+
+            Console.ReadKey();
+        }
+
+    }
+
+
+}
