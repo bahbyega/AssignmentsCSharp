d043e2e3097fb44e7e1e44c24505ef147ae1f6b5
Denis Porsev <den.porsev@gmail.com>
2020-09-21 17:03:33 +0300

    Methods ready. No testing.

---
+++ HW-3/LINQtask/BusinessLogic.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQtask
+{
+    class BusinessLogic
+    {
+        private List<User> users = new List<User>();
+        private List<Record> records = new List<Record>();
+
+        public BusinessLogic()
+        {
+            // наполнение обеих коллекций тестовыми данными
+        }
+
+        public List<User> GetUsersBySurname(String surname)
+        {
+            var usersToFind = (from user in users
+                               where user.Surname == surname
+                               select user).ToList();
+            return usersToFind;
+        }
+
+        public User GetUserByID(int id)
+        {
+            var userToFind = (from user in users
+                              where user.ID == id
+                              select user).ToList();
+
+            // will throw expception if multiple users found
+            User oneFoundUser = userToFind.Single();
+            return oneFoundUser;
+        }
+
+        public List<User> GetUsersBySubstring(String substring)
+        {
+            var usersToFind = (from user in users
+                               where (user.Name.Contains(substring) ||
+                                      user.Surname.Contains(substring))
+                               select user).ToList();
+            return usersToFind;
+        }
+
+        public List<String> GetAllUniqueNames()
+        {
+            var uniqueNames = from user in users
+                              select user.Name + " " + user.Surname;
+            uniqueNames.Distinct();
+            return (List<string>)uniqueNames; // cast from IEnumerable to List
+        }
+
+        public List<User> GetAllAuthors()
+        {
+            var authors = (from record in records
+                           where record.Message != null
+                           select record.Author).ToList(); // there are going
+                                                           // to be the same author multiple times
+            return authors;
+        }
+
+        public Dictionary<int, User> GetUsersDictionary()
+        {
+            var map = users.GroupBy(user => user.ID) // groups users by key property
+                           .ToDictionary(user => user.Key, user => user.First()); // key is IGrouping property
+            return map;
+        }
+
+        public int GetMaxID()
+        {
+            int maxID = (from user in users
+                         select user.ID).Max();
+            return maxID;
+        }
+
+        public List<User> GetOrderedUsers()
+        {
+            var usersSorted = users.OrderBy(user => user.ID);
+            return (List<User>)usersSorted;
+        }
+
+        public List<User> GetDescendingOrderedUsers()
+        {
+            var usersSorted = users.OrderByDescending(user => user.ID);
+            return (List<User>)usersSorted;
+        }
+
+        public List<User> GetReversedUsers()
+        {
+            var usersReversed = (from user in users
+                                 select user).Reverse();
+            return (List<User>)usersReversed;
+        }
+
+        public List<User> GetUsersPage(int pageSize, int pageIndex)
+        {
+            var usersPage = users.Skip(pageSize).Take(pageIndex);
+            return (List<User>)usersPage;
+        }
+    }
+}
---
+++ HW-3/LINQtask/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQtask
+{
+    class User
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+        public String Surname { get; set; }
+        public User(int id, String name, String surname)
+        {
+            this.ID = id;
+            this.Name = name;
+            this.Surname = surname;
+        }
+        public override string ToString()
+        {
+            return string.Format("ID={0}: {1} {2}", ID, Name, Surname);
+        }
+    }
+}
