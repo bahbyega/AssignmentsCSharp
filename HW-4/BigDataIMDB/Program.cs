2fea67dfd38aaccf949907b37da1293f92684a33
Denis Porsev <den.porsev@gmail.com>
2020-10-11 14:16:36 +0300

    Initial commit

---
+++ HW-4/BigDataIMDB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BigDataIMDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
